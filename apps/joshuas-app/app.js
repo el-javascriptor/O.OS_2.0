4227a7ae9fc80cf8b2debb74742d944d36bcb6a1
el-javascriptor <owenjoshuabryant3@gmail.com>
2025-04-05 20:12:02 -0400

    more stuff

--- apps/joshuas-app/app.js
+++ apps/joshuas-app/app.js
@@ -1,3 +1,26 @@
+/*
+	soupsoupsoupsoupsoup!
+
+	Here's some example code to get you started. Also, note that
+	you will have to first install your app in the operating system
+	terminal using `sudo app install <yourappname>`, at the current moment:
+
+		sudo apt install joshuas-app
+
+	Also, you'll want to use devtools to put it in a phone shape because
+	my RWD is *struggling* ;)
+
+	The O.OS uses localStorage() to store data (what apps are downloaded,
+	files, settings, etc.)
+
+	If your app wishes to store data, all the keys to the data should be prefixed
+	by your app name. EX:
+
+		NOT: localStorage.setItem("username", "owen");
+		YES: localStorage.setItem("joshuas-app-username", "owen")
+*/
+
+
 //this name must be the same as the folder title, and the component name
 export const app_name = "joshuas-app";
 
